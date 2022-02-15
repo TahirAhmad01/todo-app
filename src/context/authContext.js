ec069a638d7747d438804dc23a538c6828c6dd51
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-02-15 23:08:25 +0600

    login fix

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -4,41 +4,51 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import "../firebase/config";
 
 const AuthContext = createContext();
-// const cookies = new Cookies();
-
+const hasLogin = localStorage.getItem("todoList");
+// const cookies = new Cookies()
 export function useAuth() {
   return useContext(AuthContext);
 }
 
 export const AuthProvider = ({ children }) => {
   const [login, setLogin] = useState(false);
+  const [error, setError] = useState("");
   const [userInfo, setInfo] = useState({
     name: "",
     email: "",
     profileIco: "",
   });
-  const [error, setError] = useState("");
 
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
 
+  //google auth login function
   const GoogleLogin = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
-        // console.log({ user });
-        setInfo({
-          name: user.displayName,
-          email: user.email,
-          profileIco: user.photoURL,
-        });
+        const displayName = user.displayName;
+        const email = user.email;
+        const photoURL = user.photoURL;
+        const userObj = {
+          name: displayName,
+          email: email,
+          userImg: photoURL,
+          login: true,
+        };
         setLogin(true);
-        // cookies.set("Name", setInfo.name, { path: "/" });
-        // console.log(cookies.get("Name"));
+        localStorage.setItem("todoList", JSON.stringify(userObj));
+        const hasLogin2 = localStorage.getItem("todoList");
+        const obj2 = JSON.parse(hasLogin2);
+        setInfo({
+          name: obj2.name,
+          email: obj2.email,
+          profileIco: obj2.userImg,
+        });
       })
       .catch((error) => {
         const errorMessage = error.message;
@@ -46,12 +56,28 @@ export const AuthProvider = ({ children }) => {
         console.log(errorMessage);
       });
   };
+  //login check & set userData
+  useEffect(() => {
+    if (hasLogin) {
+      const obj = JSON.parse(hasLogin);
+      if (obj.login === true) {
+        setLogin(true);
+        setInfo({
+          name: obj.name,
+          email: obj.email,
+          profileIco: obj.userImg,
+        });
+      }
+    }
+  }, []);
 
+  //logout function
   function logout() {
     signOut(auth)
       .then((res) => {
         // Sign-out successful.
         setLogin(false);
+        localStorage.removeItem("todoList");
       })
       .catch((error) => {
         const errorMessage = error.message;
