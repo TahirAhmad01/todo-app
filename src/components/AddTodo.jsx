9b15146ffc89a9862fe1b62cffedb07655f5a6f5
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-02-18 00:40:36 +0600

    add task modal added

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,7 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
+import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import * as React from "react";
 
@@ -9,11 +10,9 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  borderRadius: "7px",
   boxShadow: 24,
-  p: 4,
 };
 
 export default function AddTodo() {
@@ -30,21 +29,54 @@ export default function AddTodo() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          {/* <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography> */}
-          <h3 style={{ textAlign: "center", marginBottom: "30px" }}>
+        <Box sx={style} className="modal_size">
+          <h2 style={{ textAlign: "center", marginBottom: "22px" }}>
             New Task
-          </h3>
+          </h2>
+
           <TextField
             helperText=" "
             id="demo-helper-text-aligned-no-helper"
-            label="Name"
+            label="Enter Task"
+            sx={{ width: "100%" }}
           />
+          <Stack component="form" noValidate spacing={3}>
+            <TextField
+              id="date"
+              label="Due Daate"
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: "100%" }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+
+            <TextField
+              id="time"
+              label="Set Time"
+              type="time"
+              defaultValue="07:30"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              sx={{ width: "100%" }}
+            />
+          </Stack>
+
+          <Stack
+            direction="row"
+            spacing={2}
+            style={{ marginTop: "30px", justifyContent: "flex-end" }}
+          >
+            <Button variant="outlined" color="error" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="contained">Save</Button>
+          </Stack>
         </Box>
       </Modal>
     </div>
